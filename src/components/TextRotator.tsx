e332bb193f1d95bf929c9689d5cf2ea19febef44
Malvin S. Eikemo <malvin.se.ei@gmail.com>
2025-01-29 08:20:48 +0000

    Edited TextRotator

--- src/components/TextRotator.tsx
+++ src/components/TextRotator.tsx
@@ -7,19 +7,19 @@ function TextRotator() {
     className="py-4  rounded-md flex flex-col justify-center items-center overflow-hidden"
   >
     <div className="font-poppins text-base sm:text-2xl [text-wrap:balance] text-gray-700">
-    I am a Software Developer &
+    I'm a
       <span
         className="inline-flex ml-2 flex-col h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] sm:h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] overflow-hidden"
       >
         <ul
           className="block text-left font-rubik text-lg sm:text-3xl leading-tight [&_li]:block animate-text-slide"
         >
-          <li className="text-[#2f7df4]">Dumb Fuck</li>
+          <li className="text-[#2f7df4]">Plumber</li>
           <li className="text-[#2f7df4]">Gamer</li>
           <li className="text-[#2f7df4]">Creator</li>
-          <li className="text-[#2f7df4]">Developer</li>
+          <li className="text-[#2f7df4]">Front End Developer</li>
           <li className="text-[#2f7df4]">Student</li>
-          <li className="text-[#2f7df4]">Lover (not atm)</li>
+          <li className="text-[#2f7df4]">Retard</li>
         </ul>
       </span>
     </div>
